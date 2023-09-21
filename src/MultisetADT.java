6986ca3a1c2660a4f9a85f8a411bfa1d766f87ec
BenjaminTSandoval <99620362+BenjaminTSandoval@users.noreply.github.com>
2023-09-21 17:45:48 -0400

    MultisetADT default scope

--- src/MultisetADT.java
+++ src/MultisetADT.java
@@ -1,9 +1,9 @@
 public interface MultisetADT {
-    public boolean add(Object item);
-    public void remove(Object item);
-    public boolean contains(Object item);
-    public boolean isEmpty();
-    public int count(Object item);
-    public int size();
+    boolean add(Object item);
+    void remove(Object item);
+    boolean contains(Object item);
+    boolean isEmpty();
+    int count(Object item);
+    int size();
 
 }
